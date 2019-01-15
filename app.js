f9443179bc6f12c009e55fac1734b8f7507ee2d0
Thirstypooch <juancocchi6@gmail.com>
2019-01-15 12:04:07 -0500

    bodyParser installation & implementation required fields to ideas/add route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -24,6 +25,10 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 //Index route
 app.get('/', (req, res) => {
     const title = 'Welcome Nito';
@@ -40,6 +45,29 @@ app.get('/about', (req, res) => {
 //Add idea form
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
+});
+
+//Process form
+app.post('/ideas', (req, res) => {
+    //console.log(req.title, req.body);
+    //res.send('ok');
+    let errors = [];
+
+    if(!req.body.title) {
+        errors.push({text: 'Please add a title'});
+    }
+    if(!req.body.details) {
+        errors.push({text: 'Please add some details'});
+    }
+    if(errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        res.send('passed');
+    }
 });
 
 const port = 5000;
