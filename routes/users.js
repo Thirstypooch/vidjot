07bf8a8ce4d7fe73b10cc2ac8bfd8300e256c006
Thirstypooch <juancocchi6@gmail.com>
2019-01-29 22:57:28 -0500

    logout implementation

--- routes/users.js
+++ routes/users.js
@@ -81,4 +81,11 @@ router.post('/register', (req, res) => {
     }
 });
 
+//Logout User
+router.get('/logout', (req, res) => {
+    req.logOut();
+    req.flash('success_msg', 'You are now logged out');
+    res.redirect('/users/login');
+});
+
 module.exports = router;
